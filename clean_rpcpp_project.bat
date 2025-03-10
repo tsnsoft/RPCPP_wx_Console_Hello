a355a0bf4c38f597c99b98de6cfa5e8c930c69bb
Talipov S.N. <proffix4@gmail.com>
2025-03-10 20:31:09 +0500

    rev. 20250310203109

--- clean_rpcpp_project.bat
+++ clean_rpcpp_project.bat
@@ -1,11 +1,13 @@
 @echo off
+setlocal enabledelayedexpansion
 
-REM Определение шаблонов файлов для удаления
-set FILE_PATTERNS=*.o *.exe *.bookmarks *.debug *.Manifest *.layout *_private.* *.win
-
-REM Удаление указанных шаблонов файлов
-for %%f in (%FILE_PATTERNS%) do (
-    del /Q %%f >nul 2>&1
-)
+for %%f in (*.o) do del /Q /F "%%f" >nul 2>&1
+for %%f in (*.exe) do del /Q /F "%%f" >nul 2>&1
+for %%f in (*.bookmarks) do del /Q /F "%%f" >nul 2>&1
+for %%f in (*.debug) do del /Q /F "%%f" >nul 2>&1
+for %%f in (*.Manifest) do del /Q /F "%%f" >nul 2>&1
+for %%f in (*.layout) do del /Q /F "%%f" >nul 2>&1
+for %%f in (*_private.*) do del /Q /F "%%f" >nul 2>&1
+for %%f in (*.win) do del /Q /F "%%f" >nul 2>&1
 
 echo Cleaning of project is completed.
